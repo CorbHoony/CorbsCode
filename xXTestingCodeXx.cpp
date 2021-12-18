8fa10b87e02b17a9e28088b51c61d2934fcb295b
CorbHoony <96320808+CorbHoony@users.noreply.github.com>
2021-12-17 19:16:06 -0500

    Add files via upload

---
+++ xXTestingCodeXx.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int age;
+    cout << "please enter 20" << endl ;
+    cin >> age;
+    if (age==20)
+    {
+        cout << "every thing seems to be working corectly." << endl ;
+    } 
+    else
+    {
+        cout << "allright this is really good!" << endl ;
+    }
+}
